26495c435c2d411efd364b73aceaec982f318ed6
tydshun <69792117+tydshun@users.noreply.github.com>
2021-01-20 12:08:00 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!
-  
+  before_action :authenticate_user!,except: [:index]
+
   def index
     # @items = Item.order('created_at DESC')
   end
