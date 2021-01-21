23b59fa9635d8684c25c5b06be76e0f2c945fbe9
tydshun <69792117+tydshun@users.noreply.github.com>
2021-01-21 16:55:58 +0900

    商品編集機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :find_params, only: [:show, :edit, :update]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -19,13 +20,28 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
-  # def edit
-  # end
+  def edit
+    if user_signed_in? && current_user.id == @item.user.id
+    else
+      redirect_to action: :index
+    end
+  end
+
+  def update
+    if @item.save
+      redirect_to action: :index
+    else
+      render :edit
+    end
+  end
 
   private
+
+  def find_params
+    @item = Item.find(params[:id])
+  end
 
   def item_params
     params.require(:item).permit(:name, :explanation, :category_id, :condition_id, :postage_type_id, :prefecture_id,
