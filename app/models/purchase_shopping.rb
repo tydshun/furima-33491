0bffe2933ac6532566f58c01680d193c59520d33
tydshun <69792117+tydshun@users.noreply.github.com>
2021-01-25 11:01:27 +0900

    不要な記述を削除

--- app/models/purchase_shopping.rb
+++ app/models/purchase_shopping.rb
@@ -1,7 +1,7 @@
 class PurchaseShopping
 
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :address, :building_name, :phone_number, :purchase, :token, :user_id, :item_id
+  attr_accessor :postal_code, :prefecture_id, :city, :address, :building_name, :phone_number, :token, :user_id, :item_id
   
   with_options presence: true do
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
